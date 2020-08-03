b257c8efca58491b82e1f64ed4999b549aef0edc
Sabil <sabilmdtv1@gmail.com>
2020-08-03 18:33:31 +0100

    completed reversestring challenge

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,20 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// function reverse(str) {
+//   return str.split("").reverse().join("");
+// }
 
+// function reverse(str) {
+//   const arr = str.split("");
+//   let reverseArr = [];
+//   for (let i = arr.length; i > 0; i++) {
+//     reverseArr.push(arr[i]);
+//   }
+//   return reverseArr.join();
+// }
+
+function reverse(str) {
+  str.split("").reduce((rev, char) => char + rev, "");
+}
 module.exports = reverse;
