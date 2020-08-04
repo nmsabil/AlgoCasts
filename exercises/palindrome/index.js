c2c70b02da8708620c506c70b4228f76e0d343c8
Sabil <sabilmdtv1@gmail.com>
2020-08-04 17:05:14 +0100

    palindrome challenge completed

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,18 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  // const reverse = str.split(""); //making array of each character
+  // const reversed = []; //initializing empty array
+  // for (let i = reverse.length; i >= 0; i--) {
+  //   //reversing array
+  //   reversed.push(reverse[i]); //pushing reversed characters to array
+  // }
+  // return reversed.join("") === reverse.join(""); //joining the array to make a string and comparing if its equal to reversed drging
+  // const reversed = str.split("").reverse().join("");
+  // return str === reversed;
+  const arr = str.split("");
+  return arr.every((el, i) => el === str[str.length - i - 1]);
+}
 
 module.exports = palindrome;
--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -10,16 +10,19 @@
 //   return str.split("").reverse().join("");
 // }
 
-// function reverse(str) {
-//   const arr = str.split("");
-//   let reverseArr = [];
-//   for (let i = arr.length; i > 0; i++) {
-//     reverseArr.push(arr[i]);
-//   }
-//   return reverseArr.join();
-// }
-
 function reverse(str) {
-  str.split("").reduce((rev, char) => char + rev, "");
+  const arr = str.split("");
+  let reverseArr = [];
+  for (let i = arr.length; i > 0; i++) {
+    reverseArr.push(arr[i]);
+    debugger;
+  }
+  return reverseArr.join();
 }
+
+reverse("apple");
 module.exports = reverse;
+
+// function reverse(str) {
+//   str.split("").reduce((rev, char) => char + rev, "");
+// }
