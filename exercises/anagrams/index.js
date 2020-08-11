6b8edece0073006273cd0e20001e53d897498e4e
Sabil <sabilmdtv1@gmail.com>
2020-08-11 20:20:00 +0100

    second solution for anagram challenge

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,38 +8,53 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
+// function anagrams(stringA, stringB) {
+//   let s1Map = {};
+//   let s2Map = {};
+
+//   let s1 = stringA.toLowerCase().replace(/[^\w]/g, "").split("");
+//   let s2 = stringB.toLowerCase().replace(/[^\w]/g, "").split("");
+
+//   charMap(s1, s1Map);
+//   charMap(s2, s2Map);
+
+//   console.log(s1Map, s2Map);
+
+//   if (Object.keys(s1Map).length !== Object.keys(s2Map).length) {
+//     return false;
+//   }
+
+//   for (let char in s1Map) {
+//     if (s1Map[char] !== s2Map[char]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// function charMap(arr, which) {
+//   for (let el of arr) {
+//     if (!arr[el]) {
+//       which[el] = 1;
+//     } else {
+//       which[el]++;
+//     }
+//   }
+// }
+
 function anagrams(stringA, stringB) {
-  let s1Map = {};
-  let s2Map = {};
+  let st1 = cleanString(stringA);
+  let st2 = cleanString(stringB);
 
-  let s1 = stringA.toLowerCase().replace(/[^\w]/g, "").split("");
-  let s2 = stringB.toLowerCase().replace(/[^\w]/g, "").split("");
-
-  charMap(s1, s1Map);
-  charMap(s2, s2Map);
-
-  console.log(s1Map, s2Map);
-
-  if (Object.keys(s1Map).length !== Object.keys(s2Map).length) {
+  if (st1 !== st2) {
     return false;
+  } else {
+    return true;
   }
-
-  for (let char in s1Map) {
-    if (s1Map[char] !== s2Map[char]) {
-      return false;
-    }
-  }
-  return true;
 }
 
-function charMap(arr, which) {
-  for (let el of arr) {
-    if (!arr[el]) {
-      which[el] = 1;
-    } else {
-      which[el]++;
-    }
-  }
+function cleanString(str) {
+  return str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("");
 }
 
 module.exports = anagrams;
