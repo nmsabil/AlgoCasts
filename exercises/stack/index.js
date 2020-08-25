42d2504010bcf0908052b201a49d0aef4dc0eb4a
Sabil <sabilmdtv1@gmail.com>
2020-08-25 19:47:39 +0100

    first queue in javascript implementation

--- exercises/stack/index.js
+++ exercises/stack/index.js
@@ -10,6 +10,19 @@
 //   s.pop(); // returns 2
 //   s.pop(); // returns 1
 
-class Stack {}
+class Stack {
+  constructor() {
+    this.record = [];
+  }
+  push(record) {
+    this.record.push(record);
+  }
+  pop() {
+    return this.record.pop();
+  }
+  peek() {
+    return this.record[this.record.length - 1];
+  }
+}
 
 module.exports = Stack;
